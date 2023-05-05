3dc2ca17693e269cf7fc6473ba8a5a7de029508c
baovtgcc200230 <baovtgcc200230@fpt.edu.vn>
2023-05-05 14:28:23 +0700

    t
    t

--- PVT_HOME REAL ESTATE/RealEstate-PHP/admin/config.php
+++ PVT_HOME REAL ESTATE/RealEstate-PHP/admin/config.php
@@ -1,6 +1,6 @@
 <?php
 
-	$con = mysqli_connect("localhost","root","","realestatephp");
+	$con = mysqli_connect("us-cdbr-east-06.cleardb.net","b61fb782a18161","f0793db9","heroku_a69fea095885345");
 	if (mysqli_connect_errno())
 	{
 		echo "Failed to connect to MySQL: " . mysqli_connect_error();
